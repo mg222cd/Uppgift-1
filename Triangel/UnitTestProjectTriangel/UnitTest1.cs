c69cef22c2a09f64654bdac859e72d0ed4c9db68
Marike Grinde <mg222cd@student.lnu.se>
2013-11-27 17:51:26 +0100

    uppdaterad fil

--- Triangel/UnitTestProjectTriangel/UnitTest1.cs
+++ Triangel/UnitTestProjectTriangel/UnitTest1.cs
@@ -6,11 +6,26 @@ namespace UnitTestProjectTriangel
     [TestClass]
     public class UnitTest1
     {
-        //Test av IsScalene
+        //TESTER FÖR KONTROLL AV DATATYP
+        //ENBART DOUBLE SKA GODTAS
         [TestMethod]
         public void TestIsScalene()
         {
             
         }
+
+        //TESTER FÖR KONTROLL AV ANTAL INMATADE VÄRDEN
+        //ENDAST 3 ST SKA GODTAS
+
+        //TESTER FÖR KONTROLL ATT VÄRDENA ÄR RIKTIGA
+        //FÅR EJ VARA NOLL ELLER NEGATIVA TAL
+
+        //TESTER FÖR ATT POSITIVA TAL UNDER 1 INTE AVRUNDAS TILL 1
+
+        //FINLIR
+        //TEST FÖR ATT VÄRDENA ÄR RIMLIGA FÖR EN TRIANGEL
+
+        //TEST AV RIMLIGA VÄRDEN
+        //SOM SKA FUNGERA
     }
 }
