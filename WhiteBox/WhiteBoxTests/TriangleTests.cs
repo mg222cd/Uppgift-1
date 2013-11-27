282a5d45d91fc46fdb6f4d407b392e347c6db269
Marike Grinde <mg222cd@student.lnu.se>
2013-11-27 18:48:32 +0100

    uppdaterat dok

---
+++ WhiteBox/WhiteBoxTests/TriangleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {   
+        //TEST FÖR DATATYP
+        //ENDAST DOUBLE SKA GODTAS
+        [TestMethod()]
+        public void TriangleTest()
+        {
+            Assert.Fail();
+        }
+
+        //TEST FÖR ANTAL INMATADE VÄRDEN
+        //ENDAST 3 ST SKA GODTAS
+
+        //TEST ATT VÄRDENA ÄR RIMLIGA
+        //FÅR EJ VARA NOLL ELLER NEGATIVA TAL
+
+        //TEST FÖR POSITIVA TAL UNDER 1
+        //SÅ ATT DE INTE AVRUNDAS TILL 1
+
+        //MATEMATISKT FINLIR
+        //TEST ATT VÄRDENA ÄR RIMLIGA FÖR EN TRIANGEL
+
+        //TEST AV RIMLIGA VÄRDEN SÅ ATT DE FUNGERAR
+        //PÅ DE 3 TYPERNA AV TRIANGLAR
+
+
+    }
+}
