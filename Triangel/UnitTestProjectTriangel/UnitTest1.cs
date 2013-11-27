7d81601541cc11852cb6bf180e4fea7fa2126a21
Marike Grinde <mg222cd@student.lnu.se>
2013-11-27 17:44:16 +0100

    ny fil för whitebox tester

---
+++ Triangel/UnitTestProjectTriangel/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectTriangel
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test av IsScalene
+        [TestMethod]
+        public void TestIsScalene()
+        {
+            
+        }
+    }
+}
